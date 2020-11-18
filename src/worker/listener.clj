8343e989cbc0a4e48e598d05d4f6a250bf56abad
filip <fbielejec@gmail.com>
2020-11-18 14:34:51 +0100

    remove async

--- src/worker/listener.clj
+++ src/worker/listener.clj
@@ -1,12 +1,9 @@
 (ns worker.listener
   (:require
    [aws.sqs :as aws-sqs]
-   [clojure.core.async :refer [close! thread <!! >!! <! go-loop go] :as async]
    [mount.core :as mount :refer [defstate]]
    [taoensso.timbre :as log]
    ))
-
-;; (defonce control-ch (async/chan))
 
 ;; TODO : invoke java parser, process message body
 (defn handle-message [body]
@@ -19,18 +16,18 @@
         {:keys [workers-queue-url]} aws
         sqs (aws-sqs/create-client aws)]
     (log/info "Starting worker listener")
-    (<!! (go-loop []
-           (try
-             ;; If the queue is empty, wait for 2 seconds and poll again
-             (log/debug "Polling...")
-             (if-let [{:keys [body receipt-handle]} (aws-sqs/get-next-message sqs workers-queue-url)]
-               (do
-                 (handle-message body)
-                 (aws-sqs/ack-message sqs workers-queue-url receipt-handle)))
-             (<! (async/timeout 2000))
-             (catch Exception e
-               (log/error "Error processing a message" {:error e})))
-           (recur)))))
+    (loop []
+      (try
+        ;; If the queue is empty, wait for 2 seconds and poll again
+        (log/debug "Polling...")
+        (if-let [{:keys [body receipt-handle]} (aws-sqs/get-next-message sqs workers-queue-url)]
+          (do
+            (handle-message body)
+            (aws-sqs/ack-message sqs workers-queue-url receipt-handle)))
+        (Thread/sleep 2000)
+        (catch Exception e
+          (log/error "Error processing a message" {:error e})))
+      (recur))))
 
 (defstate listener
   :start (start (mount/args)))
