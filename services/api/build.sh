c6c8dd76fa10b6612a2947704d3a587d6aeefab3
filip <fbielejec@gmail.com>
2020-12-10 16:04:06 +0100

    docker builds

---
+++ services/api/build.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -e
+
+NAME=api-service
+
+while getopts b:p:t: flag
+do
+  case "${flag}" in
+    b) BUILD=${OPTARG};;
+    p) PUSH=${OPTARG};;
+    t) TAG=${OPTARG};;
+  esac
+done
+
+# defaults
+
+if [ -z ${TAG+x} ];
+then
+  TAG=latest
+fi
+
+if [ -z ${BUILD+x} ];
+then
+  BUILD=true
+fi
+
+if [ -z ${PUSH+x} ];
+then
+  PUSH=false
+fi
+
+# BUILD
+
+IMG=$NAME:$TAG
+
+if [ $BUILD = true ]
+then
+    cd ../../
+    clojure -Spom;
+    clojure -A:uberjar api-service.jar -C -m api.main;    
+    docker build --tag $IMG -f services/api/Dockerfile .
+fi
+
+# PUSH
+
+if [ $PUSH = true ]
+then
+  echo "Pushing $IMG to Dockerhub"
+  # authenticate docker to use dockerhub registry
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin  
+  # tag image  
+  docker tag $IMG spread/$NAME:$TAG
+  # push to registry
+  docker push spread/$NAME:$TAG
+fi
+
+echo "Done"
+exit $?
