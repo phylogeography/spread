9a44b97807fee5b37dc501c326adb00608287665
filip <fbielejec@gmail.com>
2021-01-14 18:50:38 +0100

    cleanup

--- src/main/java/com/spread/data/Layer.java
+++ src/main/java/com/spread/data/Layer.java
@@ -7,7 +7,6 @@ import com.spread.data.attributable.Line;
 import com.spread.data.attributable.Point;
 
 import lombok.Getter;
-import lombok.experimental.Accessors;
 import lombok.ToString;
 
 @ToString(includeFieldNames=true)
@@ -65,109 +64,5 @@ public class Layer {
         this.lines = builder.lines;
         this.areas = builder.areas;
     }
-
-    // public enum Type {
-    //     tree, counts, trees
-    // }
-
-    // @Getter
-    // private final Type type;
-    // @Getter
-    // private final String id;
-    // @Getter
-    // private final String description;
-
-    // @Getter
-    // private final List<Point> points;
-    // @Getter
-    // private final List<Line> lines;
-    // @Getter
-    // private final List<Area> areas;
-
-    // @Accessors(fluent = true)
-    // @Getter
-    // private final boolean hasPoints;
-    // @Accessors(fluent = true)
-    // @Getter
-    // private final boolean hasLines;
-    // @Accessors(fluent = true)
-    // @Getter
-    // private final boolean hasAreas;
-
-    // public Layer(String id,
-    //              String description,
-    //              List<Point> points,
-    //              List<Line> lines,
-    //              List<Area> areas) {
-    //     this.type = Type.tree;
-    //     this.id = id;
-    //     this.description = description;
-    //     this.points = points;
-    //     this.lines = lines;
-    //     this.areas = areas;
-    //     this.hasPoints = true;
-    //     this.hasLines = true;
-    //     this.hasAreas = true;
-    // }
-
-    // public Layer(String id,
-    //              String description,
-    //              List<Point> points,
-    //              List<Line> lines) {
-    //     this.type = Type.tree;
-    //     this.id = id;
-    //     this.description = description;
-    //     this.points = points;
-    //     this.lines = lines;
-    //     this.areas = null;
-    //     this.hasPoints = true;
-    //     this.hasLines = true;
-    //     this.hasAreas = false;
-    // }
-
-    // public <T> Layer(String id,
-    //              String description,
-    //              List<T> collection) {
-    //     this.type = Type.counts;
-    //     this.id = id;
-    //     this.description = description;
-    //     // this.points = points;
-    //     this.lines = null;
-    //     this.areas = null;
-    //     this.hasPoints = true;
-    //     this.hasLines = false;
-    //     // this.hasAreas = false;
-
-    //     // if (T instanceof Area) {
-
-    //     // }
-
-    // }
-
-    // // public  Layer(String id,
-    // //              String description,
-    // //              List<Point> points) {
-    // //     this.type = Type.counts;
-    // //     this.id = id;
-    // //     this.description = description;
-    // //     this.points = points;
-    // //     this.lines = null;
-    // //     this.areas = null;
-    // //     this.hasPoints = true;
-    // //     this.hasLines = false;
-    // //     this.hasAreas = false;
-    // // }
-
-    // // public Layer(String id,
-    // //              String description,
-    // //              List<Area> areas) {
-    // //     this.type = Type.trees;
-    // //     this.id = id;
-    // //     this.description = description;
-    // //     this.areas = areas;
-    // //     this.hasPoints = false;
-    // //     this.hasLines = false;
-    // //     this.hasAreas = true;
-    // // }
 
 }
