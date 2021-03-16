21ff9fecab77e0d1aaca44a6f818689915c73dca
filip <fbielejec@gmail.com>
2021-03-16 13:48:11 +0100

    ui subs POC

--- src/cljs/ui/home/page.cljs
+++ src/cljs/ui/home/page.cljs
@@ -1,8 +1,19 @@
 (ns ui.home.page
   (:require [ui.router.component :refer [page]]
+            [ui.subscriptions :as subs]
             [re-frame.core :as re-frame]))
 
+;; TODO : just for graphql subs POC
+(def id "60b08880-03e6-4a3f-a170-29f3c75cb43f")
+
 (defmethod page :route/home []
-  (fn []
-    [:div
-     [:p "HOME"]]))
+  (let [authed-user (re-frame/subscribe [::subs/authorized-user])
+        parser-status (re-frame/subscribe [::subs/discrete-tree-parser id])]
+      (fn []
+        (let [{:keys [email]} @authed-user
+              {:keys [status]} @parser-status]
+            [:div
+             [:p "HOME"]
+             [:pre (str "current-user:" email)]
+             [:pre (str id " status:" status)]
+             ]))))
