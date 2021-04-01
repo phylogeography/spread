3938b299b366a5141bf2789b4710b08a98ec9105
filip <fbielejec@gmail.com>
2021-04-01 14:46:38 +0200

    wip : panes

--- src/cljs/ui/new_analysis/page.cljs
+++ src/cljs/ui/new_analysis/page.cljs
@@ -7,10 +7,59 @@
 
 ;; TODO : https://xd.adobe.com/view/cab84bb6-15c6-44e3-9458-2ff4af17c238-9feb/screen/9c18388a-e890-4be4-9c5a-8d5358d86567/
 
+(defn discrete-mcc-tree []
+  [:pre "discrete-mcc-tree"])
+
+(defn discrete-rates []
+  [:pre "discrete-rates"])
+
+(defn continuous-mcc-tree []
+  [:pre "continuous-mcc-tree"])
+
+(defn continuous-time-slices []
+  [:pre "continuous-time-slices"])
+
 (defmethod page :route/new-analysis []
   (let [active-page (re-frame/subscribe [::router.subs/active-page])]
     (fn []
-      (log/debug "app-layout/active-page" active-page)
-      [app-container
-       [:div.new-analysis
-        [:p "NEW-ANALYSIS"]]])))
+      (let [{:keys [query]} @active-page
+            {active-tab :tab} query]
+
+        [app-container
+         [:div.new-analysis
+          [:span "Run new analysis"]
+          [:div.tabbed-pane
+           [:div.tabs
+            (map (fn [tab]
+                   [:button.tab {:class (when (= active-tab tab) "active")
+                                 :key tab
+                                 :on-click #(re-frame/dispatch [:router/navigate :route/new-analysis nil {:tab tab}])}
+                    (case tab
+                      "discrete-mcc-tree"
+                      [:div
+                       [:span "Discrete"]
+                       [:span "MCC tree"]]
+
+                      "discrete-rates"
+                      [:div
+                       [:span "Discrete"]
+                       [:span "Rates"]]
+
+                      "continuous-mcc-tree"
+                      [:div
+                       [:span "Continuous"]
+                       [:span "MCC tree"]]
+
+                      "continuous-time-slices"
+                      [:div
+                       [:span "Continuous"]
+                       [:span "Time slices"]]
+                      nil)])
+                 ["discrete-mcc-tree" "discrete-rates" "continuous-mcc-tree" "continuous-time-slices"])]
+           [:div.panel
+            (case active-tab
+              "discrete-mcc-tree"      [discrete-mcc-tree]
+              "discrete-rates"         [discrete-rates]
+              "continuous-mcc-tree"    [continuous-mcc-tree]
+              "continuous-time-slices" [continuous-time-slices]
+              [discrete-mcc-tree])]]]]))))
