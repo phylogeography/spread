99bb912e42ba919a82f682f81d7c557ca79d1bdf
Juan Monetta <jpmonettas@gmail.com>
2021-04-26 09:30:31 -0300

    Refactor zoom-bar into slider generic component

--- src/cljs/analysis_viewer/components.cljs
+++ src/cljs/analysis_viewer/components.cljs
@@ -10,3 +10,15 @@
      [:span.on "On"]
      [:span.block {:class (if on? "block-on" "block-off")}]
      [:span.off "Off"]]))
+
+(defn slider [{:keys [zoom-perc zoom-inc-fn zoom-dec-fn class]}]
+  [:div.slider {:class class}
+   [:button {:on-click zoom-inc-fn} "+"]
+   [:div {:width "6px" :height "100%"}
+    [:svg 
+     [:line {:x1 "10" :y1 "100" :x2 "10" :y2 "0" :stroke "#DEDEE8" :stroke-width 3}]
+     [:line {:x1 "10" :y1 "100" :x2 "10" :y2 "0" :stroke "#EEBE53" :stroke-width 3
+             :stroke-dasharray 100
+             :stroke-dashoffset zoom-perc}]
+     [:rect {:x "4" :y (str (- zoom-perc 6)) :width "12" :height "12" :fill "white" :stroke "grey"}]]]
+   [:button {:on-click zoom-dec-fn} "-"]])
