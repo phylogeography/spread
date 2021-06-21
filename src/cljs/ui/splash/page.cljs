1be701cd673f205299a5bc0bb5e5c24005dc9cc1
filip <fbielejec@gmail.com>
2021-06-21 11:47:02 +0200

    wip : use hooks

--- src/cljs/ui/splash/page.cljs
+++ src/cljs/ui/splash/page.cljs
@@ -4,9 +4,46 @@
             [ui.router.component :refer [page]]
             [ui.router.subs :as router.subs]
             [ui.subscriptions :as subs]
+            [reagent-material-ui.styles :as styles]
+            [reagent-material-ui.core.card :refer [card]]
+            [reagent-material-ui.core.card-content :refer [card-content]]
+            [reagent-material-ui.core.typography :refer [typography]]
+            [reagent-material-ui.core.container :refer [container]]
+            ["react" :as react]
             [ui.utils :as utils :refer [<sub >evt]]))
 
+;; https://github.com/mui-org/material-ui/blob/master/docs/src/pages/getting-started/templates/sign-in/SignIn.js
+
+(def use-styles (styles/make-styles (fn [{:keys [spacing] :as theme}]
+                                      {:paper {:margin-top     (spacing 8)
+                                               :display       "flex"
+                                               :flex-direction "column"
+                                               :align-items    "center"}
+
+
+
+                                       })))
+
+
 (defmethod page :route/splash []
+  (let [{:keys [google]}                 (<sub [::subs/config])
+        {:keys [query]}                  (<sub [::router.subs/active-page])
+        {:keys [client-id redirect-uri]} google
+        _ (react/useEffect (fn []
+                             (log/debug "component-did-mount" query)
+                             (when-let [code (:code query)]
+                               (case (-> query :auth keyword)
+                                 :google
+                                 (>evt [:splash/send-google-verification-code code redirect-uri])
+                                 nil))))
+        classes (use-styles)]
+
+         (prn "@ style classes" classes)
+
+
+    ))
+
+#_(defmethod page :route/splash []
   (let [{:keys [google]}                 (<sub [::subs/config])
         {:keys [query]}                  (<sub [::router.subs/active-page])
         {:keys [client-id redirect-uri]} google]
@@ -21,7 +58,24 @@
                                   nil)))
        :reagent-render
        (fn []
-         [:div
+         ;; let [classes (use-styles)]
+
+
+         ;; (prn "@ style classes" classes)
+
+         [container {:component "main"
+                     :max-width "xs"}
+          [card
+           [card-content
+
+            [typography "Spread"]
+
+            ]
+
+
+           ]
+          ]
+         #_[:div
           [:a.button {:href
                       (str "https://accounts.google.com/o/oauth2/v2/auth"
                            "?client_id=" client-id
