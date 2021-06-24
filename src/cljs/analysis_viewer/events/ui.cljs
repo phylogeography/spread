7f2d31522e070f22df17b0c69e8ced25f6d4d6ff
Juan Monetta <jpmonettas@gmail.com>
2021-06-24 15:35:58 -0300

    Use material ui sliders in most places

--- src/cljs/analysis_viewer/events/ui.cljs
+++ src/cljs/analysis_viewer/events/ui.cljs
@@ -1,7 +1,19 @@
-(ns analysis-viewer.events.ui)
+(ns analysis-viewer.events.ui
+  (:require [shared.utils :as utils]))
 
 (defn toggle-collapsible-tab [db [_ parent-id tab-id]]
-  (update-in db [:ui.collapsible-tabs/tabs parent-id tab-id] not))
+  (let [tab-new-state (not (get-in db [:ui.collapsible-tabs/tabs parent-id tab-id]))
+        close-all-tabs (fn [db]
+                         (update db :ui.collapsible-tabs/tabs
+                                 (fn [sections-map]
+                                   (utils/map-map-vals sections-map
+                                                       (fn [tabs-map]
+                                                         (utils/map-map-vals tabs-map (constantly false)))))))]
+    ;; close all the tabs before toggleing so we don't have more than one collapsible tab open at a time
+    ;; this is so we don't overflow vertical space. Making it scrollable is tricky, since we can't fix a max-height
+    (-> db
+        close-all-tabs 
+        (assoc-in [:ui.collapsible-tabs/tabs parent-id tab-id] tab-new-state))))
 
 (defn toggle-switch-button [db [_ button-id]]
   (update-in db [:ui.switch-buttons/states button-id] not))
