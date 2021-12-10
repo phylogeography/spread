2c547ed072b4333e76bfeb3a0f7a211efc0c1d10
filip <fbielejec@gmail.com>
2021-12-10 21:07:58 +0100

    addressing comments

--- src/cljs/ui/splash/page.cljs
+++ src/cljs/ui/splash/page.cljs
@@ -1,7 +1,7 @@
 (ns ui.splash.page
   (:require ["react" :as react]
             [reagent-material-ui.core.avatar :refer [avatar]]
-            [reagent-material-ui.core.button :refer [button]]
+            ;; [reagent-material-ui.core.button :refer [button]]
             [reagent-material-ui.core.card :refer [card]]
             [reagent-material-ui.core.card-content :refer [card-content]]
             [reagent-material-ui.core.card-header :refer [card-header]]
@@ -12,6 +12,7 @@
             [reagent-material-ui.styles :as styles]
             [reagent.core :as reagent]
             [taoensso.timbre :as log]
+            [shared.components :refer [button spread-logo]]
             [ui.component.icon :refer [arg->icon icons]]
             [ui.component.input :refer [text-input]]
             [ui.router.component :refer [page]]
@@ -21,148 +22,71 @@
 
 (def email-pattern #"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
 
-(def use-styles (styles/make-styles (fn [_]
-                                      {:grid {:background "#ECEFF8"
-                                              :min-width  "100%"
-                                              :min-height "100vh"}
-
-                                       :card {:min-width     "720px"
-                                              :box-shadow    "0px 30px 60px #313B5833"
-                                              :border-radius "20px"}
-
-                                       :card-header {:margin-left "35%"}
-
-                                       :card-header-title {:text-align     "left"
-                                                           :font           "normal normal 900 30px/35px Roboto"
-                                                           :letter-spacing "3.9px"
-                                                           :color          "#3A3668"
-                                                           :text-transform "uppercase"}
-
-                                       :card-content {:display         "flex"
-                                                      :flex-direction  "column"
-                                                      :align-items     :center
-                                                      :justify-content :space-evenly}
-
-                                       :sign-in {:text-align :center
-                                                 :font       "normal normal 900 24px/28px Roboto"
-                                                 :color      "#3A3668"
-                                                 :margin     "10px"
-                                                 :padding    "10px"}
-
-                                       :sign-in-sub {:width      "440px"
-                                                     :text-align :left
-                                                     :font       "normal normal medium 16px/19px Roboto"
-                                                     :color      "#757295"
-                                                     :margin     "10px"
-                                                     :padding    "10px"}
-
-                                       :send-button {:width          "325px"
-                                                     :height         "50px"
-                                                     :text-align     :center
-                                                     :font           "normal normal medium 16px/19px Roboto"
-                                                     :text-transform :none
-                                                     :color          "white"
-                                                     :background     "#3428CA"}
-
-                                       :google-button {:width          "325px"
-                                                       :height         "50px"
-                                                       :background     "#F0F0F0 0% 0% no-repeat padding-box"
-                                                       :border         "1px solid #F0F0F0"
-                                                       :border-radius  "8px"
-                                                       :text-transform :none
-                                                       :font           "normal normal medium 16px/27px Roboto"
-                                                       :color          "#757575"}
-
-                                       })))
-
 (defmethod page :route/splash []
   (let [{:keys [google root-url]}               (<sub [::subs/config])
         {{:keys [code token] :as query} :query} (<sub [::router.subs/active-page])
         {:keys [client-id redirect-uri]}        google
         email?                                  (fn [email]
                                                   (and (string? email) (re-matches email-pattern email)))
-        [email setEmail]                        (react/useState nil)
-        [error setError]                        (react/useState false)
-        _                                       (react/useEffect (fn []
-                                                                   (log/debug "component-did-mount" query)
+        email                                   (reagent/atom nil)
+        set-email                               #(reset! email %)
+        error                                   (reagent/atom nil)
+        set-error                               #(reset! error %)
+        on-mount                                (fn []
+                                                  (log/debug "component-did-mount" query)
+                                                  (case (-> query :auth keyword)
+                                                    :google
+                                                    (when code
+                                                      (>evt [:splash/google-login code redirect-uri]))
+                                                    :email
+                                                    (when token
+                                                      (>evt [:splash/email-login token]))
+                                                    nil))]
+    (fn []
+      (on-mount)
+      (if (:auth query) ;; if we have :auth key on query means google is redirecting so show a loading-spinner
+        [:div.loading-spinner
+         [circular-progress {:size 100}]]
+        [:div.splash
+         [:div.card
+          [:div.card-header
+           [spread-logo]]
+          [:span.sign-in
+           "Sign in with email"]
+          [:span.sign-in-sub
+           "Enter your email address. We will send you a special link that you can sign in with instantly."]
+          [text-input {:label       "E-mail address"
+                       :opts        {:style {:height "50px"}
+                                     :class "email-input"}
+                       :error?      @error
+                       :helper-text (when @error
+                                      "Enter valid email address")
+                       :value       @email
+                       :on-change   (fn [value]
+                                      (if (email? value)
+                                        (set-error false)
+                                        (set-error true))
+                                      (set-email value))}]
+          [button {:text      "Send magic link"
+                   :class     "send-button"
+                   :disabled? (or (nil? @email) @error)
+                   :on-click  (fn []
+                                (>evt [:splash/send-login-email @email root-url])
+                                (set-email nil))}]
+          [:h2 {:style {:width         "325px"
+                        :text-align    "center"
+                        :border-bottom "2px solid #757295"
+                        :line-height   "0.1em"
+                        :margin        "30px 0 30px"}}
+           [:span {:style {:background "#fff"
+                           :padding    "0 10px"
+                           :color      "#757295"}} "or"]]
+          [:a.button.clickable {:class "google-button"
+                                :href  (str "https://accounts.google.com/o/oauth2/v2/auth"
+                                            "?client_id=" client-id
+                                            "&scope=email%20profile"
+                                            "&response_type=code"
+                                            "&redirect_uri=" (utils/url-encode redirect-uri))}
 
-                                                                   (case (-> query :auth keyword)
-                                                                     :google
-                                                                     (when code
-                                                                       (>evt [:splash/google-login code redirect-uri]))
-
-                                                                     :email
-                                                                     (when token
-                                                                       (>evt [:splash/email-login token]))
-
-                                                                     nil)))
-        classes (use-styles)]
-    (if (:auth query) ;; if we have :auth key on query means google is redirecting so show a loading-spinner
-      [:div.loading-spinner
-       [circular-progress {:size 100}]]
-
-      [grid {:class-name  (:grid classes)
-             :container   true
-             :spacing     0
-             :align-items "center"
-             :justify     "center"}
-
-       [card {:class-name (:card classes)}
-
-        [card-header {:class-name (:card-header classes)
-                      :avatar     (reagent/as-element [avatar {:alt     "spread"
-                                                               :variant "square"
-                                                               :src     (arg->icon (:spread icons))}])
-                      :title      (reagent/as-element [typography {:class-name (:card-header-title classes)}
-                                                       "Spread"])}]
-
-        [divider {:variant "fullWidth"}]
-
-        [card-content {:class-name (:card-content classes)}
-
-         [typography {:class-name (:sign-in classes)}
-          "Sign in with email"]
-
-         [typography {:class-name (:sign-in-sub classes)}
-          "Enter your email address. We will send you a special link that you can sign in with instantly."]
-
-         [text-input {:label       "E-mail address"
-                      :opts        {:style {:width   "325px"
-                                            :margin  "10px"
-                                            :padding "10px"}}
-                      :error?      error
-                      :helper-text (when error
-                                     "Enter valid email address")
-                      :value       email
-                      :on-change   (fn [value]
-                                     (if (email? value)
-                                       (setError false)
-                                       (setError true))
-                                     (setEmail value))}]
-
-         [button {:class-name (:send-button classes)
-                  :variant    "contained"
-                  :disabled   error
-                  :on-click   (fn []
-                                (>evt [:splash/send-login-email email root-url])
-                                (setEmail nil))}
-          "Send magic link"]
-
-         [:h2 {:style {:width         "325px"
-                       :text-align    "center" ;
-                       :border-bottom "2px solid #757295"
-                       :line-height   "0.1em"
-                       :margin        "30px 0 30px"}}
-          [:span {:style {:background "#fff"
-                          :padding    "0 10px"
-                          :color      "#757295"}} "or"]]
-
-         [button {:class-name (:google-button classes)
-                  :variant    "contained"
-                  :start-icon (reagent/as-element [:img {:src (arg->icon (:google icons))}])
-                  :href       (str "https://accounts.google.com/o/oauth2/v2/auth"
-                                   "?client_id=" client-id
-                                   "&scope=email%20profile"
-                                   "&response_type=code"
-                                   "&redirect_uri=" (utils/url-encode redirect-uri))}
-          "Continue with Google"]]]])))
+           [:img {:src (arg->icon (:google icons))}]
+           [:span "Continue with Google"]]]]))))
