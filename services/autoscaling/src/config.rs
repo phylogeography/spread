07a74abb2e743a6da579cd0e557424697ab82ee6
filip <fbielejec@gmail.com>
2021-12-27 16:45:04 +0100

    wip : sending metric data

--- services/autoscaling/src/config.rs
+++ services/autoscaling/src/config.rs
@@ -3,12 +3,31 @@ use std::env;
 #[derive(Default, Debug, Clone)]
 pub struct Config {
     pub queue_url: String,
+    pub ecs_cluster: String,
+    pub ecs_service: String,
+    pub queue_name: String,
+    pub metric_name: String,
+    pub metric_namespace: String,
 }
 
 impl Config {
     pub fn load() -> Self {
         Config {
-            queue_url: get_env_var("WORKERS_QUEUE_URL", None),
+            queue_url: get_env_var(
+                "WORKERS_QUEUE_URL",
+                Some(
+                    "https://sqs.us-east-2.amazonaws.com/816997647674/spread-prod-worker"
+                        .to_owned(),
+                ),
+            ),
+            ecs_cluster: get_env_var("ECS_CLUSTER", Some("spread-ecs-prod".to_owned())),
+            ecs_service: get_env_var("ECS_SERVICE", Some("spread-worker".to_owned())),
+            queue_name: get_env_var("QUEUE_NAME", Some("spread-prod-worker".to_owned())),
+            metric_name: get_env_var("METRIC_NAME", Some("BacklogPerECSTask".to_owned())),
+            metric_namespace: get_env_var(
+                "METRIC_NAMESPACE",
+                Some("ECS-SQS-Autoscaling".to_owned()),
+            ),
         }
     }
 }
--- services/autoscaling/src/main.rs
+++ services/autoscaling/src/main.rs
@@ -1,7 +1,11 @@
 mod config;
 
 use aws_config::meta::region::RegionProviderChain;
-use aws_sdk_cloudwatch::{Client as CloudwatchClient, Error as CloudwatchError};
+use aws_sdk_cloudwatch::{
+    model::{Dimension, MetricDatum},
+    Client as CloudwatchClient, Error as CloudwatchError,
+};
+use aws_sdk_ecs::Client as EcsClient;
 use aws_sdk_sqs::{model::QueueAttributeName, Client as SqsClient, Error as SqsError};
 use config::Config;
 use log::{debug, info};
@@ -11,12 +15,20 @@ use std::env;
 async fn main() -> Result<(), anyhow::Error> {
     init_logging();
 
-    let Config { queue_url } = Config::load();
+    let Config {
+        queue_url,
+        ecs_cluster,
+        ecs_service,
+        metric_namespace,
+        metric_name,
+        queue_name,
+        ..
+    } = Config::load();
 
     let aws_config = aws_config::load_from_env().await;
     let cloudwatch_client = CloudwatchClient::new(&aws_config);
-
     let sqs_client = SqsClient::new(&aws_config);
+    let ecs_client = EcsClient::new(&aws_config);
 
     // aws sqs get-queue-attributes --queue-url "https://sqs.us-east-2.amazonaws.com/816997647674/spread-prod-worker" --attribute-names All
 
@@ -37,7 +49,47 @@ async fn main() -> Result<(), anyhow::Error> {
 
     info!("messages_in_flight {}", messages_in_flight);
 
-    // aws ecs list-tasks --cluster $ECS_CLUSTER --service-name $ECS_SERVICE
+    // aws ecs list-tasks --cluster spread-ecs-prod --service-name spread-worker
+
+    let tasks_count = ecs_client
+        .list_tasks()
+        .cluster(ecs_cluster)
+        .service_name(ecs_service)
+        .send()
+        .await?
+        .task_arns
+        .expect("Could not list ecs service tasks")
+        .len() as u32;
+
+    info!("tasks count {}", tasks_count);
+
+    //$( ( ($ApproximateNumberOfMessages / $NUMBER_TASKS) + ($ApproximateNumberOfMessages % $NUMBER_TASKS > 0) ) )
+
+    let backlog_per_worker = messages_in_flight / tasks_count;
+
+    info!("backlog_per_worker {}", backlog_per_worker);
+
+    // aws cloudwatch put-metric-data --namespace ECS-SQS-Autoscaling --metric-name BacklogPerECSTask --unit None --value 1 --dimensions SQS-Queue=spread-prod-worker
+
+    let dimension = Dimension::builder()
+        .set_name(Some("SQS-Queue".to_owned()))
+        .set_value(Some(queue_name))
+        .build();
+    let datum = MetricDatum::builder()
+        .set_metric_name(Some(metric_name))
+        .set_unit(None)
+        .set_value(Some(backlog_per_worker as f64))
+        .set_dimensions(Some(vec![dimension]))
+        .build();
+    let data = vec![datum];
+
+    cloudwatch_client
+        .put_metric_data()
+        .set_metric_data(Some(data))
+        .set_namespace(Some(metric_namespace))
+        .send()
+        .await
+        .expect("Could not send metric data");
 
     Ok(())
 }
