f5825e9251127eaf967714b08dbd1bd9b61695a6
filip <fbielejec@gmail.com>
2023-05-02 12:22:22 +0200

    don't use origin

--- src/clj/api/server.clj
+++ src/clj/api/server.clj
@@ -60,9 +60,9 @@
   (swap! ips assoc ip new-state))
 
 (defn ip-jail-decorator [resolver-fn]
-  (fn [{{x-forwarded-for "headers.x-forwarded-for"
-         origin          "origin"} :headers :as context} args value]
-    (let [ip            (or x-forwarded-for origin "127.0.0.1")
+  (fn [{{x-forwarded-for         "x-forwarded-for"
+         headers.x-forwarded-for "headers.x-forwarded-for"} :headers :as context} args value]
+    (let [ip            (or x-forwarded-for headers.x-forwarded-for "127.0.0.1")
           now           (System/currentTimeMillis)
           current-state (or (@ips ip) (init-state ip))
           _             (log/info "verifying IP state" current-state)
